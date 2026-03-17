d3be1aeb869a9fabe3931095d32cc51ab8404ead
JAGANATHAN V <v.jaganathan222@gmail.com>
2026-03-17 18:33:13 +0530

    TASK 1

---
+++ Task_1/Internship/Internship/src/main/java/com/RD_Technology/Internship/Application.java
@@ -0,0 +1,13 @@
+package com.RD_Technology.Internship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ Task_1/Internship/src/test/java/com/RD_Technology/Internship/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RD_Technology.Internship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
